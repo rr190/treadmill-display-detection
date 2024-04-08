2643f4e0b3abea0a0a2a5bce481a2041f0c9e5ab
visionglass <rr190cs@gmail.com>
2024-04-08 22:29:20 +0800

    modify

--- app.py
+++ app.py
@@ -1,4 +1,50 @@
 import streamlit as st
-st.title('My First Streamlit App')
+# import pymongo
+# from pymongo.server_api import ServerApi
+import pandas as pd
+from ultralytics import YOLO
+from PIL import Image
+from util import get_img, return_pred, return_board, return_nums, combine_labels, reshape
+import streamlit_authenticator as stauth
 
-st.write("Welcome")
+st.title('Gym Tracker')
+st.write("Record Your Miles!")
+
+#Get Image Data
+# buf = st.camera_input("Take a picture!")
+buf = st.file_uploader("Choose a file")
+
+#Predict
+if buf is not None:
+    model = YOLO("best3.pt")
+    img = get_img(buf)
+
+    #Retrieve Predicted Data
+    boards, numbers, labels= return_pred( model, img)
+    board = return_board(boards)
+    combined_nums = return_nums(numbers, board)
+    combined_labels = combine_labels(labels)
+    print(combined_labels)
+    print(combined_nums)
+
+    col1, col2 = st.columns(2)
+    labels = ["Time", "Calories", "Distance", "Pace","Speed", "Incline", "Heart-Rate"]
+    num_restrain = [(100, 0), (1000, 0), (10, 0), (100, 0), (30, 0), (100, 0), (1000, 10)]
+    # max_len = max(len(combined_labels),len(combined_nums)) 
+    #Turn into a table and graph
+    with col1:
+        for i, each in enumerate(combined_labels):
+            if each[0] == "Time Elapsed" or each[0] == "Time-Remaining":
+                each[0] = "Time"
+            idx = labels.index(each[0])
+            if i < len(combined_nums):
+                combined_nums[i] = reshape(combined_nums[i], num_restrain[idx][0], num_restrain[idx][1])
+            option = st.selectbox(
+            "Label", options=labels, index= idx, key=i, label_visibility="hidden")
+
+
+    with col2:
+        for each in combined_nums:
+            num = st.number_input(
+                "Number", float(each) ,label_visibility="hidden"
+            )
--- util.py
+++ util.py
@@ -1,173 +1,158 @@
+# IMPORTS
 import matplotlib.pyplot as plt
 import os
 from PIL import Image
 import cv2
 import numpy as np
 
-def return_list(model, img):
+def get_img(TEST_PATH):
+    """""
+    Reads the image and resize it to 640x640
+    Input:
+        TEST_PATH: String
+    Output:
+        img: Image of shape ((640, 640, ,3))
+    """""
+    img = Image.open(TEST_PATH)
+    img = img.resize((640, 640))
+    return img
+
+def return_pred(model ,img):
     res = model.predict(img)[0]
 
-    xyxy = {}
-
+    boards = []
+    numbers = []
+    labels = []
     for box in res.boxes:
         num = str(res.names[int(box.cls[0])])
-        if num not in xyxy.keys():
-            xyxy[num] = [(box.xyxy, box.conf)]
-        else:
-            xyxy[num].append((box.xyxy, box.conf))
-        
-    blank_img = np.array(img.copy())
+        if num == 'board':
+            boards.append((box.xyxy, box.conf))
+        elif num.isdigit() or num == ".":
+            numbers.append((box.xyxy, box.conf, num))
+        else: 
+            labels.append((box.xyxy, box.conf, num))
+    return boards, numbers, labels
 
-    numbers = []
-    label_list = []
-    for key in xyxy.keys():
+def iou(xyxy1, xyxy2):
 
-        label = key
-        
-        for item in xyxy[key]:
-            x, y, x1, y1 = item[0][0]
-            x = int(x)
-            y = int(y)
-            x1 = int(x1)
-            y1 = int(y1)
-            
-            if not label.isdigit() and label != 'board' and label != '.':
-                label_list.append((item[0][0], item[1], label))
-            elif label != 'board':
-                numbers.append((item[0][0], item[1], label))
-            blank_img = cv2.rectangle(blank_img, (int(x), int(y)), (int(x1), int(y1)), (0, 255, 0), 2)
-            blank_img = cv2.putText(blank_img, label, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (36,255,12), 2)
+    x, y, x1, y1 = xyxy1
+    x2, y2, x21, y21 = xyxy2
 
-    return label_list, numbers, xyxy, blank_img
-def return_lists(TEST_PATH, model):
-    '''''
-    Input:
-        TEST_PATH: path to the images to be tested
-        model: YOLOv8 .pt files
+    overlap = 0
+    if (x1 > x2) and (y1 > y2):
+        numerator = (x1 - x2) * (y1 - y2)
+        x_ = (x1 - x2) * (y1 - y)
+        y_ = (x1 - x2) * (y21 * y2)
+        denom = x_ * y_
+        overlap = numerator / denom
+    if overlap > 0:
+        return True
+    return False
 
-    Output:
-        boards: (len(images), # of boards detected, 2)
-            (tensor(xyxy), tensor(conf))
-        numbers_list: (len(images), # of numbers detected, 3)
-            (tensor(xyxy), tensor(conf), label)
-        blank_imgs: (len(images), 640, 640, 3)
-    '''''
-    for root, _, filenames in os.walk(TEST_PATH):
+def return_board(boards):
 
-        blank_imgs = []
-        boards = []
-        labels = []
-        numbers_list = []
-        for f in filenames:
-            img = Image.open(os.path.join(root, f)).resize((640, 640))
-            label_list, numbers, xyxy, blank_img = return_list(model, img)
-            boards.append(xyxy['board'])
-            labels.append(label_list)
-            numbers_list.append(numbers)
-            blank_imgs.append(blank_img)
+    # Return the one with the highest confidence
+    boards = sorted(boards, key= lambda x: x[-1])
+    return boards[0]
 
-    return boards, numbers_list, blank_imgs, labels
+def return_nums(numbers, board):
 
-def plot_image(nrows, ncols, images, filename):
-    '''''
-    Input:
-        nrows: # of rows for the plot
-        ncols: # of cols for the plot
-        images: array of images
-        filename: filename for the plot
+    include = ''
+    xb, yb, x1b, y1b = board[0][0]
+    xb = int(xb) - 20
+    yb = int(yb) - 20
+    x1b = int(x1b) + 20
+    y1b = int(y1b) + 20
 
-    Saves the plot
-    '''''
-    fig, _ = plt.subplots(nrows, ncols, figsize=(10, 7))
+    #Sort by conf
+    n = sorted(numbers, key=lambda x: x[-1])
 
-    for i in range(1, nrows*ncols+1):
-        fig.add_subplot(nrows, ncols, i)
-        plt.imshow(images[i-1])
-        plt.axis("off")
+    #Sort by x1
+    sorted_list = sorted(n, key=lambda x: x[0][0][0])
 
-    plt.savefig(filename)
+    # min_dist = 10
+    #Remove overlapped detections
+    unique_list = []
+    xyxy = sorted_list[0][0][0]
+    prevx, prevy, prevx1, prevy1 =xyxy
+    unique_list.append(sorted_list[0])
+    total_i = 0
+    if sorted_list[0][-1] != ".":
+        mean_width = prevx1 - prevx
+    for i in range(1, len(sorted_list)):
+        x, y, x1, y1 = sorted_list[i][0][0]
+        # if not (sorted_list[i][-1] == '.' and abs(prevx1 - x) > 4) and (abs(prevx - x) < 5 or abs(prevy - y) < 5 or abs(prevx1 - x1) < 5 or abs(prevy1 - y1) < 5):
+        # if not (sorted_list[i][-1] == '.' and abs(prevx1 - x) > 4) and (abs(prevx - x) < min_dist):
+        #     continue
+        if sorted_list[i][-1] != ".":
+            mean_width += x1 - x
+            total_i += 1
+
+        if sorted_list[i][-1] != "." and iou([prevx, prevy, prevx1, prevy1],sorted_list[i][0][0]):
+            continue
+        prevx = x
+        prevy = y
+        prevx1 = x1
+        prevy1 = y1
+        unique_list.append(sorted_list[i])
+    print(mean_width, total_i)
     
-def combine_numbers(numbers_list, boards):
-    '''''
-    Input:
-       boards: (len(images), # of boards detected, 2)
-            (tensor(xyxy), tensor(conf))
-        numbers_list: (len(images), # of numbers detected, 3)
-            (tensor(xyxy), tensor(conf), label)
-    Output:
-        included: list of combined strings
-    '''''
-    all_included = []
-    for j in range(len(numbers_list)):
-
-        b = boards[j]
-        include = ''
-        xb, yb, x1b, y1b = b[0][0][0]
-        xb = int(xb) - 20
-        yb = int(yb) - 20
-        x1b = int(x1b) + 20
-        y1b = int(y1b) + 20
-        n = numbers_list[j]
-        n = sorted(n, key=lambda x: x[0][0])
-        sorted_list = sorted(n, key=lambda x: x[0][0])
-
-        #Remove overlapped detections
-        unique_list = []
-
-        prevx =sorted_list[0][0][0]
-        prevy =sorted_list[0][0][1]
-        prevx1 =sorted_list[0][0][2]
-        prevy1 =sorted_list[0][0][3]
-        unique_list.append(sorted_list[0])
-        for i in range(1, len(sorted_list)):
-            x, y, x1, y1 = sorted_list[i][0]
-            if not (sorted_list[i][2] == '.' and abs(prevx1 - x) > 4) and (abs(prevx - x) < 1 and abs(prevy - y) < 1 and abs(prevx1 - x1) < 1 and abs(prevy1 - y1) < 1):
-                continue
-            prevx = x
-            prevy = y
-            prevx1 = x1
-            prevy1 = y1
-            unique_list.append(sorted_list[i])
-
-        # Put spacings respectively
-        prevx = 0
-        prevy = 0
-        prevx1 = 0
-        prevy1 = 0 
-        included = []
-
-        for each in unique_list:
-            x, y, x1, y1 = each[0]
-            if ((x - prevx1) > 20 and prevx != 0) :
+    mean_width /= total_i
+    print(mean_width)
+    # Put spacings respectively
+    prevx = 0
+    prevy = 0
+    prevx1 = 0
+    prevy1 = 0 
+    included = []
+    for each in unique_list:
+        x, y, x1, y1 = each[0][0]
+        if ((x - prevx1) > mean_width and prevx != 0) :
+            if include != "":
                 included.append(include)
                 include = ""
-            if (x >= xb and x1 <= x1b and y >= yb and y1<= y1b):
-                include += each[2]
-            prevx = x
-            prevy = y
-            prevx1 = x1
-            prevy1 = y1
+        if (iou(board[0][0], each[0][0]) > 0.5) :
+            include += each[2]
+        prevx = x
+        prevy = y
+        prevx1 = x1
+        prevy1 = y1
 
-        if include != "":
-            included.append(include)
-        all_included.append(included)
+    if include != "":
+        included.append(include)
 
-    return all_included
+    return included
+
 
 def combine_labels(labels):
 
-    combined_labels = []
-    combined_extended_labels = []
-    for each in labels:
-        l = sorted(each, key=lambda x: x[0][0])
-        combined_extended_labels.append(l)
-        lbls = []
-        # for e in l:
-        #     if e[1] > .5:
-        #         lbls.append(e[-1])
-        lbls.append(l)
-        combined_labels.append(lbls)
-    return combined_extended_labels, combined_labels
+    labels = sorted(labels, key=lambda x: x[-1])
+    labels = sorted(labels, key=lambda x: x[0][0][0])
+    
+    out = []
+    prev = labels[0]
+    out.append([prev[-1]])
+    i = 0
+    for label in labels[1:]:
+        
+        if (label[0][0][0] - prev[0][0][0]) < 5:
+            out[-1].append(label[-1])
+        else:
+            out.append([label[-1]])
+        prev = label
 
+    return out
+
+def reshape(num, maxm, minm):
+    num = float(num)
+    print(num, maxm, minm)
+    if num > maxm:
+        while num > maxm:
+            num /= 10
+    elif num < minm:
+        while num < minm:
+            num *= 10
+
+    return str(num)
 # if __name__ == "__main__":
 #     main()
